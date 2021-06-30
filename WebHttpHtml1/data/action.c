ba62d9780f8ca816f1771dc6d32d20b61d55d885
skhandual07 <sujata.pt02@gmail.com>
2021-06-30 20:16:05 +0530

    LR script added

---
+++ WebHttpHtml1/data/action.c
@@ -0,0 +1,97 @@
+Action()
+{
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	web_add_cookie("dw=1; DOMAIN=www.levi.in");
+
+	web_add_cookie("dw_cookies_accepted=1; DOMAIN=www.levi.in");
+
+	web_add_cookie("_sdsat_externalcampaign=; DOMAIN=www.levi.in");
+
+	web_add_auto_header("Accept-Language", 
+		"en-US,en;q=0.9");
+
+	web_add_cookie("_gcl_au=1.1.836284247.1625059163; DOMAIN=www.levi.in");
+
+	web_add_cookie("s_fid=43CEDF9FC57DB7B7-16383A1CF5E4EADF; DOMAIN=www.levi.in");
+
+	web_add_cookie("s_cc=true; DOMAIN=www.levi.in");
+
+	web_add_cookie("dw_levis_store_newsletterpopup=seen; DOMAIN=www.levi.in");
+
+	web_add_cookie("__cq_uuid=bcLb8sQIeYwmKfaksl8x7XcdQe; DOMAIN=www.levi.in");
+
+	web_add_cookie("__cq_seg=0~0.00!1~0.00!2~0.00!3~0.00!4~0.00!5~0.00!6~0.00!7~0.00!8~0.00!9~0.00; DOMAIN=www.levi.in");
+
+	web_add_cookie("_fbp=fb.1.1625059198598.1709025691; DOMAIN=www.levi.in");
+
+	web_url("www.levi.in", 
+		"URL=https://www.levi.in/", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/v1625021329000/custom-css/local-sitewide-css.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/css/all.css", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/levis_sprite_icons_size24.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Light.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Bold.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/bagicon24x24.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/levis_sprite_icons_size62.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Regular.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/webfonts/fa-solid-900.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/css/all.min.css", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dwa29815be/images/homepage/India.jpg", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw96cbad45/images/homepage/logo.jpg", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/triangledown24x24.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dwe1eaa8df/images/homepage/Summer_Daze_Desktop_OPTION.jpg", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw897754a7/images/ShopMenDesktop.png", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw5460d126/images/ShopWomen%20Desktop.png", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw1e8da55f/images/homepage/Main%20Carousel/Website%20Banner.jpg", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw693ea05e/images/ShopSustanabiliy%20Desktop.png", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dw4a88ddc6/images/Offer%20banner%20.jpg", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/interface/ui-overlay-shadow.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/dwdd89ba1c/images/homepage/newsletterpopup_background.jpg", ENDITEM, 
+		"Url=/on/demandware.store/Sites-LeviIN-Site/en_IN/__Analytics-Start?url=https%3A%2F%2Fwww.levi.in%2F&res=1366x768&cookie=1&ref=&title=Levi%27s%C2%AE%20Jeans%2C%20Jackets%20%26%20Clothing&pdf=1&qt=0&realp=0&wma=0&dir=0&fla=0&java=0&gears=0&ag=0&dwac=0.8701721655565808&cmpn=&tz=Asia/Kolkata&pcc=INR&pct=&pcat=&dw_dnt=0", ENDITEM, 
+		LAST);
+
+	web_url("Home-Newsletterpopup", 
+		"URL=https://www.levi.in/on/demandware.store/Sites-LeviIN-Site/en_IN/Home-Newsletterpopup?format=ajax", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://www.levi.in/", 
+		"Snapshot=t2.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_start_transaction("sigin_in");
+
+	web_url("login", 
+		"URL=https://www.levi.in/login", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=https://www.levi.in/", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		EXTRARES, 
+		"Url=/on/demandware.static/-/Sites-LeviIN-Library/en_IN/v1625021329000/custom-css/local-sitewide-css.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/css/all.css", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/levis_sprite_icons_size24.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/images/bagicon24x24.png", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Light.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/webfonts/fa-solid-900.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/font-awesome/5.0.0/css/all.min.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Regular.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/fonts/Interstate-Bold.woff", "Referer=https://www.levi.in/on/demandware.static/Sites-LeviIN-Site/-/en_IN/v1625021329000/css/style.css", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/dwb1a699d2/images/googleicon24x24.png", ENDITEM, 
+		"Url=/on/demandware.static/Sites-LeviIN-Site/-/en_IN/dwf2aaf995/images/facebookmonochrome24x24.png", ENDITEM, 
+		"Url=/on/demandware.store/Sites-LeviIN-Site/en_IN/__Analytics-Start?url=https%3A%2F%2Fwww.levi.in%2Flogin&res=1366x768&cookie=1&ref=https%3A%2F%2Fwww.levi.in%2F&title=Account%20Login&pdf=1&qt=0&realp=0&wma=0&dir=0&fla=0&java=0&gears=0&ag=0&dwac=0.28804518863183715&cmpn=&tz=Asia/Kolkata&pcc=INR&pct=&pcat=&dw_dnt=0", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("sigin_in",LR_AUTO);
+
+	lr_think_time(10);
+
+	return 0;
+}
